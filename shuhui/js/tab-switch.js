0bba73119a6ea22f56d79458858b7047d1774b48
jack <365898996@qq.com>
2017-06-06 16:19:20 +0800

    add shuhui

---
+++ shuhui/js/tab-switch.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+	var nav = document.getElementById("nav-left");
+	var nav_list = nav.getElementsByTagName("a");
+	var body = document.getElementsByTagName("body");
+	var show = body.getElementsByTagName('div')
+}
